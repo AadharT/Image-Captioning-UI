21fac7a897b07b199f8c8a6d1381da33be6342d1
Aadhar <tyagi.aadhar@gmail.com>
2020-04-23 00:50:57 +0530

    upload button working

--- AutoCaption/Form1.cs
+++ AutoCaption/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AutoCaption
@@ -15,6 +16,22 @@ namespace AutoCaption
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = opnfd.FileName;
+                pictureBox1.Image = new Bitmap(opnfd.FileName);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
